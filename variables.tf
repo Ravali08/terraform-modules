9485280e60f374519afb4031e0b55ed050af06f3
Ravali08 <163737862+Ravali08@users.noreply.github.com>
2024-06-23 14:15:34 +0530

    Add files via upload
    s3

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
+  type        = string
+}
+
+variable "enable_versioning" {
+  description = "Enable versioning for the S3 bucket"
+  type        = bool
+  default     = true
+}
+
+variable "tags" {
+  description = "A map of tags to assign to the bucket"
+  type        = map(string)
+  default     = {}
+}
